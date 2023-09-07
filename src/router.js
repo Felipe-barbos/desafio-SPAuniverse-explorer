d035c4531c2b254f829aa6a383a3942f2af55684
Felipe-barbos <felipe.csultimate@gmail.com>
2023-09-07 13:54:13 -0300

    feat:Desafio Concluído

--- src/router.js
+++ src/router.js
@@ -22,10 +22,27 @@ export class Router {
 
     const route = this.routes[pathname] || this.routes[404];
 
+    this.alterBackground(route);
+
     fetch(route)
       .then((data) => data.text())
       .then(html => {
         document.querySelector(".app").innerHTML = html;
       });
+  }
+
+  alterBackground(pathname) {
+    switch (pathname) {
+      case "/pages/universe.html":
+        document.querySelector("body").style.backgroundImage = "url('./assets/mountains-universe-2.png')";
+        break;
+      case "/pages/exploration.html":
+        document.querySelector("body").style.backgroundImage = "url('./assets/mountains-universe-3.png')";
+        break;
+      default:
+        document.querySelector("body").style.backgroundImage = "url('./assets/mountains-universe-1.png')";
+
+    }
+
   }
 }
